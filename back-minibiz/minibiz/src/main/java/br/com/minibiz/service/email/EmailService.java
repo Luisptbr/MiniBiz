13f29d4b950a7e8a523f11907a82b547f86c8f81
Luisptbr <luis_henrique2411@hotmail.com>
2024-10-31 00:51:18 -0300

    Adicionando end-points, Esqueci a senha e Redefinir senha

---
+++ back-minibiz/minibiz/src/main/java/br/com/minibiz/service/email/EmailService.java
@@ -0,0 +1,32 @@
+package br.com.minibiz.service.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import br.com.minibiz.model.user.User;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendPasswordResetEmail(User user, String token) {
+        String subject = "Reset Password";
+        String resetUrl = "http://localhost:4200/reset-password?token=" + token;
+        String message = "Click on the following link to reset your password: " + resetUrl;
+
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(user.getEmail());
+        email.setSubject(subject);
+        email.setText(message);
+
+        mailSender.send(email);
+    }
+}
